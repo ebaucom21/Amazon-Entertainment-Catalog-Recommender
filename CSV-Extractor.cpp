71922330922055656e699c55bd1f17bba2a2e954
ebaucom21 <elijah@elijahbau.com>
2025-04-15 23:54:25 -0400

    CSV-Extractor finished as well as the created a tester

--- CSV-Extractor.cpp
+++ CSV-Extractor.cpp
@@ -38,79 +38,64 @@ class csvExtraction {
                 size_t pos = 0;
                 int lastPos = 0;
                 // discard show_id
-                lastPos = line.find(',');
+                getCell(line, lastPos); 
                 // Get type
-                pos = line.find(',', lastPos + 1);
-                cell = line.substr(lastPos + 1, pos - lastPos - 1);
-                row[1].push_back(cell); // Type
-                lastPos = pos;
+                row[1] = getCell(line, lastPos);
                 // Get title
-                pos = line.find(',', lastPos + 1);
-                cell = line.substr(lastPos + 1, pos - lastPos - 1);
-                row[0].push_back(cell); // Title
-                lastPos = pos;
+                row[0] = getCell(line, lastPos);
                 // Get actors/directors
                 // Get director first
-                pos = line.find(',', lastPos + 1);
-                cell = line.substr(lastPos + 1, pos - lastPos - 1);
-                row[2].push_back(cell); // Director
-                lastPos = pos;
-                // Get actors
-                pos = line.find('\"', lastPos + 2);
-                cell = line.substr(lastPos + 2, pos - lastPos - 3);
-                size_t lastPos2 = 0;
-                size_t pos2 = 0;
-                while (cell.find(',', pos2+1) != string::npos) {
-                    pos2 = cell.find(',', pos2 + 1);
-                    if (pos2 != string::npos) {
-                        string curAct = cell.substr(lastPos2, pos2);
-                        row[2].push_back(curAct); // Actor
-                        lastPos2 = pos2 + 1;
-                    }
-                }
-                string curAct = cell.substr(lastPos2, cell.size() - lastPos2);
-                row[2].push_back(curAct); // Actor
-                lastPos = pos + 2;
+                row[2] = getCell(line, lastPos);
+                // Get the actors inside of the quotes that are seperated by commas
+                vector<string> temp = getCell(line, lastPos);
+                temp.insert(temp.begin(), row[2][0]); // Add the director to the front of the vector
+                row[2] = temp;
+                
                 // Skip country
-                pos = line.find(',', lastPos + 1);
-                lastPos = pos;
+                getCell(line, lastPos);
                 // Skip date_added
-                pos = line.find(',', lastPos + 1);
-                lastPos = pos;
+                getCell(line, lastPos);
                 // Skip release_year
-                pos = line.find(',', lastPos + 1);
-                lastPos = pos;
+                getCell(line, lastPos);
                 // Get rating
-                pos = line.find(',', lastPos + 1);
-                cell = line.substr(lastPos + 1, pos - lastPos - 1);
-                row[3].push_back(cell); // Rating
-                lastPos = pos;
+                row[3] = getCell(line, lastPos);
                 // Get duration
-                pos = line.find(',', lastPos + 1);
-                cell = line.substr(lastPos + 1, pos - lastPos - 1);
-                row[4].push_back(cell); // Duration
+                row[4] = getCell(line, lastPos);
                 // Get genres the same as actors
-                pos = line.find('\"', lastPos + 2);
-                cell = line.substr(lastPos + 2, pos - lastPos - 3);
-                lastPos2 = 0;
-                pos2 = 0;
-                while (cell.find(',', pos2+1) != string::npos) {
-                    pos2 = cell.find(',', pos2 + 1);
-                    if (pos2 != string::npos) {
-                        string curGen = cell.substr(lastPos2, pos2);
-                        row[5].push_back(curGen); // Genre
-                        lastPos2 = pos2 + 1;
-                    }
-                }
-                string curGen = cell.substr(lastPos2, cell.size() - lastPos2);
-                row[5].push_back(curGen); // Genre
-                lastPos = pos + 2;
+                row[5] = getCell(line, lastPos);
                 // Skip description
-                pos = line.find(',', lastPos + 1);
-                lastPos = pos;
+                getCell(line, lastPos);
+
+                result.push_back(row); // Add the row to the result
             }
 
             return result;
         }
-
+        vector<string> getCell(const string& line, int& lastPos) {
+            vector<string> cells;
+            if (line.find('\"', lastPos + 1) == lastPos + 1) {
+                int pos = line.find('\"', lastPos + 1);
+                lastPos = pos;
+                pos = line.find('\"', lastPos + 1);
+                string cell = line.substr(lastPos + 1, pos - lastPos - 1);
+                int lastPos2 = 0;
+                int pos2 = 0;
+                while (cell.find(',', lastPos2+1) != string::npos) {
+                    pos2 = cell.find(',', lastPos2 + 1);
+                    string curInd = cell.substr(lastPos2, pos2 - lastPos2);
+                    cells.push_back(curInd);
+                    lastPos2 = pos2 + 1;
+                }
+                string curInd = cell.substr(lastPos2, cell.size() - lastPos2);
+                cells.push_back(curInd);
+                lastPos = pos + 1;
+            } else {
+                // If the cell is not in quotes, find the next comma
+                size_t pos = line.find(',', lastPos + 1);
+                string cell = line.substr(lastPos + 1, pos - lastPos - 1);
+                cells.push_back(cell);
+                lastPos = pos;
+            }
+            return cells;
+        }
 };
